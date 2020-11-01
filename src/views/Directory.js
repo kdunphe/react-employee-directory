0f79690b3e24adf77fd35af6916be1bfd3b7312d
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-11-01 17:15:26 -0500

    Setting up app

---
+++ src/views/Directory.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header";
+// import Container from "../components/Container";
+// import Footer from "../components/Footer";
+
+function Directory() {
+    return (
+        <div>
+            <Header />
+            {/* <Container />
+            <Footer /> */}
+        </div>
+    );
+}
+
+export default Directory;
